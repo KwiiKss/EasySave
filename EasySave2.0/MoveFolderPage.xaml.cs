dd33d40b92b86b024526d0991d8700c4d44da917
KwiiKss <lrent.kevin@gmail.com>
2022-11-30 11:07:06 +0100

    Create movefolderpage movefilepage

---
+++ EasySave2.0/MoveFolderPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace EasySave2._0
+{
+    /// <summary>
+    /// Logique d'interaction pour MoveFolderPage.xaml
+    /// </summary>
+    public partial class MoveFolderPage : Window
+    {
+        public MoveFolderPage()
+        {
+        }
+    }
+}
